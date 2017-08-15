fadeb4d9574deb6a3e58dfe9785bbc20972fcb79
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-15 21:15:02 +1000

    images added

--- index.android.js
+++ index.android.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  Dimensions,
   Image,
   AppRegistry,
   StyleSheet,
@@ -14,17 +15,11 @@ import picTanner from './assets/Tanner-McTab.png'
 
 export default class HelloWorld extends Component {
   render() {
-      console.log("__________hello__________")
+      console.log("==============")
     return (
       /** <View style={styles.container}> **/
       /** <Text style={styles.welcome}> **/
       <View style={styles.container}>
-        <StatusBar hidden={true} />
-        <Text style={styles.defaultText}>Sierra</Text>
-        <Text style={[styles.defaultText,styles.selectedText]}>
-          Tanner
-        </Text>
-        <Text style={styles.defaultText}>Travis</Text>
         <Image style={styles.pic} source={picSierra}>
           <Text style={styles.userName}>Sierra Spencer</Text>
         </Image>
@@ -39,13 +34,8 @@ export default class HelloWorld extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'flex-start',
+    alignItems: 'center',
     justifyContent: 'space-around',
-    /** justifyContent: 'space-around' || 'space-between' */
-    justifyContent: 'space-around',
-    flexDirection: 'row',
-    backgroundColor: '#DDD'
   },
   defaultText: {
     fontSize: 22,
@@ -60,7 +50,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'yellow',
     color: 'blue',
     fontWeight: 'bold'
+  },
+  pic: {
+    /**borderRadius: 50,*/
+    flex:1,
+    width: Dimensions.get('window').width,
+    resizeMode: 'cover'
+  },
+  userName: {
+    backgroundColor: 'rgba(0,0,0,.7)',
+    fontSize: 30,
+    padding: 10,
+    color: 'white'
   }
+
+
 });
 
 
