722bb0f43079b30bfd7f5adc65894cf833788d0f
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-22 21:25:54 +1000

    ch 5-3 completed.

--- ch5-3_ColorList/components/ColorInfo.js
+++ ch5-3_ColorList/components/ColorInfo.js
@@ -5,24 +5,47 @@ import {
   StyleSheet
 } from 'react-native'
 
+import ColorTools from 'color'
+
 /*
 *   the entire props is passed to the stateless functional component as an argument
 *
 */
-const ColorInfo = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text style={styles.text}>{ navigation.state.params.color }</Text>
-  </View>
-)
+const ColorInfo = ({ navigation }) => {
+  const color = ColorTools(navigation.state.params.color)
 
+
+  return (
+    <View style={ [styles.container, { backgroundColor: color }] }>
+      <Text style={ [styles.text, { color: color.negate()}] }>
+        { color.hex() }
+      </Text>
+      <Text style={ [styles.text, { color: color.negate()}] }>
+        { color.rgb().string() }
+      </Text>
+      <Text style={ [styles.text, { color: color.negate()}] }>
+        { color.hsl().string() }
+      </Text>
+    </View>
+  )
+}
 
 /*
 *
 *     NB: this is a stateless functional component
 */
+/*
+*     old method: set ColorInfo.navigationOptions to an object
 ColorInfo.navigationOptions = {
   title: "Color Details"
 }
+*     new method: set ColorInfo.navigationOptions to a function
+*     the function gets the navigation object
+*     once the function has the navigation object it extracts the value for color
+*/
+ColorInfo.navigationOptions = ({ navigation }) => ({
+  title: navigation.state.params.color
+})
 
 
 const styles = StyleSheet.create({
