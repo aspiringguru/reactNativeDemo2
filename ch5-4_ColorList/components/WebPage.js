358712c1c70343eaa0c26cd01052ac4d1e444a37
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-22 22:23:40 +1000

    ch 5-5 complete.

--- ch5-4_ColorList/components/WebPage.js
+++ ch5-4_ColorList/components/WebPage.js
@@ -5,10 +5,20 @@ import {
   StyleSheet
 } from 'react-native'
 
-
-const WebPage = () => (
+/*
+*   https://facebook.github.io/react-native/docs/webview.html
+*   
+*   onPress action on Text object styled as button in ColorForm
+*   passes value for uri
+*   nb: navigation.state.params only contains value for uri
+*   so { uri: navigation.state.params.uri } can be contracted to
+*   navigation.state.params
+*   contentInset={{ top: -500 }} is supposed to position the page 500 pixels down.
+*/
+const WebPage = ({ navigation }) => (
   <WebView style={styles.container}
-           source= {{uri: "https://www.github.com"}}  />
+          source= { navigation.state.params}
+          contentInset={{ top: -500 }}  />
 )
 
 WebPage.navigationOptions = {
